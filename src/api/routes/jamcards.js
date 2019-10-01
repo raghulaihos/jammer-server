101cd0102ea974c144ce92f7aff87192b23355a1
Raghuram Subramanian <raghulaihos@gmail.com>
2019-09-30 23:10:30 -0700

    Update jamcards.js

--- src/api/routes/jamcards.js
+++ src/api/routes/jamcards.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const jamcards_controller = require('../controllers/jamcards');
 const is_auth = require('../middleware/is_auth');
 
-router.get('/jamcards', jamcards_controller.get_jamcards);
+router.get('/jamcards', is_auth, jamcards_controller.get_jamcards);
 
 router.get('/test', jamcards_controller.test);
 
