89a2b6574aa53097898b306efcda15cfbe07d948
Raghuram Subramanian <raghulaihos@gmail.com>
2019-10-14 02:15:40 -0700

    added book and slot routes

--- src/api/controllers/search.js
+++ src/api/controllers/search.js
@@ -12,7 +12,6 @@ const search_all_cards = async (req, res, next) => {
 }
 
 const jamroom_details = async (req, res, next) => {
-    console.log(req.query.room_name);
     try {
         let result = await db.query(`select equipment from jam_rooms where name='${req.query.room_name}'`);
         res.status(200).json(result.rows[0]);
@@ -20,11 +19,39 @@ const jamroom_details = async (req, res, next) => {
         const error = new Error('DB Error fetching equipment');
         error.statusCode = 500;
         next(err);
-    }   
+    }
 }
 
+const slots = async (req, res, next) => {
+    try {
+        let result = await db.query(`select j.name ,t.slots from time_slots t
+                                     left join jam_rooms j on t.room_id=j.room_id where j."name"='${req.query.room_name}' `)
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        const error = new Error('DB Error fetching time slots');
+        error.statusCode = 500;
+        next(err);
+    }
+}
+
+const book = async(req,res,next)=>{
+    let push_obj={};
+    req.body.slots.forEach((val,i) => {
+       if(val==='booked'){
+           val='closed'
+       }
+        push_obj[i]=val;
+    });
+    push_obj = JSON.stringify(push_obj);
+    let result = await db.query(`update time_slots t  
+    set slots='${push_obj}'
+    from jam_rooms j where "name"='${req.body.room_name}' and t.room_id=j.room_id returning t.slots`)
+    res.status(200).send(result);
+}
 
 module.exports = {
     search_all_cards,
-    jamroom_details
+    jamroom_details,
+    slots,
+    book
 };
--- src/api/routes/search.js
+++ src/api/routes/search.js
@@ -5,6 +5,8 @@ const is_auth = require('../middleware/is_auth');
 
 router.get('/search', is_auth, search_controller.search_all_cards);
 router.get('/room_details', is_auth, search_controller.jamroom_details);
+router.get('/slots', is_auth, search_controller.slots);
+router.put('/book', is_auth, search_controller.book);
 
 
 module.exports = router;
